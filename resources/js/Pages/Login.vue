6fd5dfd7169bd1ea69459473ad8aaddda80ff3dc
Russel Manito <manito.russel@gmail.com>
2022-09-28 22:11:44 +0800

    color fix

--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -19,17 +19,17 @@
             <form>
                 <div class="mb-2.5">
                     <label for="email" class="lg:gray text-lg ml-5 lg:poppins">Email</label><br/>
-                    <input type="text" class="text-[1.625rem] w-[25rem] lg:gray lg:inter rounded-lg pl-5" id="email" placeholder="user@email.com"/>
+                    <input type="text" class="text-[1.625rem] w-[25rem] lg:gray lg:inter rounded-lg pl-5 bg-[#EAF0F7]" id="email" placeholder="user@email.com"/>
                 </div>
                 <div class="mb-7">
                     <label for="password" class="lg:gray text-lg ml-5 lg:poppins">Password</label><br/>
-                    <input type="password" class="text-[1.625rem] w-[25rem] lg:gray lg:inter rounded-lg pl-5" id="password" placeholder="********" />
+                    <input type="password" class="text-[1.625rem] w-[25rem] lg:gray lg:inter rounded-lg pl-5 bg-[#EAF0F7]" id="password" placeholder="********" />
                 </div>
                 <div class="mb-[3.75rem]">
                     <router-link to="/forgot-password" class="lg:gray text-lg underline ml-5">Forgot Password?</router-link>
                 </div>
                 <div>
-                    <button id="submit" class="btn btn-primary rounded-2xl w-full h-[3.75rem] text-lg font-semibold lg:poppins" style="background-color:#E05534"> Login </button>
+                    <button id="submit" class="btn btn-primary rounded-2xl w-full h-[3.75rem] text-lg font-semibold lg:poppins" style="background-color:#E05534; border: none;"> Login </button>
                 </div>
             </form>
         </div>
